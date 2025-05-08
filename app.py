77b08d012e2ccd042a46f7c99b9cd0e0b0f2ea64
DafneCT <l24213231@tectijuana.edu.mx>
2025-05-08 14:58:30 -0700

    Update app.py
    cambio

--- app.py
+++ app.py
@@ -3,3 +3,5 @@ from time import sleep
 while True:
     sleep(10)
     print("Testing new pr using default namespace...")
+user_message = input("Please enter a message: ")
+print ("The message you entered is:")
